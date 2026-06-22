b2e8c887414d46a7a7a687381f1334f1d5ea917f
tchralts-boop <tchralts@gmail.com>
2026-06-23 00:02:14 +0530

    Add component index file for easy imports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Hero } from './Hero';
+export { default as Collections } from './Collections/Collections';
+export { default as LatestDrop } from './LatestDrop/LatestDrop';
+export { default as Story } from './Story/Story';
+export { default as Footer } from './Footer/Footer';
